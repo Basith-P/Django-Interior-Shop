87d2cc23c0957df7634470ce2ede22a7cf4c2ef7
Basith P <basithpofficial@gmail.com>
2023-09-21 03:11:07 +0530

    feat: static files

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import frontpage
+from . import views
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', frontpage, name='frontpage')
+    path('', views.frontpage, name='frontpage'),
+    path('contact/', views.contact, name='contact')
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'core/front_page.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
