271543f4bd53567b974e00db98bbd2d41d929e6f
Basith P <basithpofficial@gmail.com>
2023-09-21 23:06:17 +0530

    feat: category page

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -6,4 +6,5 @@ app_name = "products"
 
 urlpatterns = [
     path("<slug:cat_slug>/<slug:prod_slug>/", views.product, name="product"),
+    path("<slug:cat_slug>/", views.category, name="category"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -2,13 +2,13 @@ import random
 
 from django.shortcuts import get_object_or_404, render
 
-from .models import Product
+from .models import Category, Product
 
 
 def product(request, cat_slug, prod_slug):
     product = get_object_or_404(Product, slug=prod_slug, category__slug=cat_slug)
 
-    similar_products = list(product.category.products.exclude(id=product.id))
+    similar_products = list(product.category.products.exclude(id=product.id))  # type: ignore
     if len(similar_products) > 4:
         similar_products = random.sample(similar_products, 4)
 
@@ -17,3 +17,15 @@ def product(request, cat_slug, prod_slug):
         "similar_products": similar_products,
     }
     return render(request, "products/product.html", context)
+
+
+def category(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    products = category.products.all()  # type: ignore
+
+    context = {
+        "category": category,
+        "products": products,
+    }
+
+    return render(request, "products/category.html", context)
