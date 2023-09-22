24897612a6ddf66062c26d64f2cc0746ddc6aa26
Basith P <basithpofficial@gmail.com>
2023-09-22 21:30:28 +0530

    feat: cart details page

--- interiorshop/settings.py
+++ interiorshop/settings.py
@@ -74,6 +74,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "apps.products.context_processors.menu_categories",
+                "apps.cart.context_processors.cart",
             ],
         },
     },
--- interiorshop/urls.py
+++ interiorshop/urls.py
@@ -6,6 +6,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("apps.core.urls", namespace="core")),
+    path("cart/", include("apps.cart.urls", namespace="cart")),
     path("vendors/", include("apps.vendor.urls", namespace="vendor")),
     path("", include("apps.products.urls", namespace="products")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
