fb4497ae6c37ff95082fe2141d312d9fca583435
Basith P <basithpofficial@gmail.com>
2023-09-22 22:15:14 +0530

    feat: update cart item quantity

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -6,8 +6,15 @@ from .cart import Cart
 def cart_view(request):
     cart = Cart(request)
     remove_item = request.GET.get("remove")
+    change_quantity = request.GET.get("change_quantity")
+    quantity = request.GET.get("quantity")
+
     if remove_item:
         cart.remove(remove_item)
-
         return redirect("cart:cart")
+
+    if change_quantity:
+        cart.add(change_quantity, quantity, True)
+        return redirect("cart:cart")
+
     return render(request, "cart/cart_details.html")
