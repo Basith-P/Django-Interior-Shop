4d11b871d88e28863fe18e89ca1014e91194f1ff
Basith P <basithpofficial@gmail.com>
2023-09-22 22:11:40 +0530

    feat: remove item from cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .cart import Cart
 
 
 def cart_view(request):
+    cart = Cart(request)
+    remove_item = request.GET.get("remove")
+    if remove_item:
+        cart.remove(remove_item)
+
+        return redirect("cart:cart")
     return render(request, "cart/cart_details.html")
